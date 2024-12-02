992f559586fb302050f749ebf0d9023546d742c8
OeunSochetra <oeunsochetra@gmail.com>
2024-12-02 15:53:54 +0700

    "Refactor area index view: updated conditional rendering, removed unused variables, and simplified computed properties"

--- src/views/area/index.vue
+++ src/views/area/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="md:w-[1184px] w-full mb-[10rem]">
     <Loading></Loading>
-    <div v-if="findIdMatch">
-      <Title :title="matchingFeatureJobs[0].categories[0].name" />
+    <div v-if="matchingFeatureJobs">
+      <Title :title="matchingTitle" />
       <div
-        class="flex flex-col gap-4"
+        class="mt-6"
         v-for="(item, index) in matchingFeatureJobs"
         :key="index"
       >
@@ -19,9 +19,6 @@
         />
       </div>
     </div>
-    <div class="flex items-center justify-center" v-else>
-      <Title title="No Data Found" />
-    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -29,7 +26,6 @@ import { useRoute } from "vue-router";
 import { useFeatureJobStore } from "../../store/featureJobStore";
 import { storeToRefs } from "pinia";
 import { computed, onMounted, ref } from "vue";
-import { IFeatureJob } from "../../constants/common";
 import CardJobs from "../../components/CardJobs.vue";
 import Title from "../../components/Title.vue";
 import Loading from "../../components/Loading.vue";
@@ -42,23 +38,22 @@ const route = useRoute();
 
 const isLoading = ref<boolean>(false);
 
-const categories = computed(() => {
-  return featureJobList.value.map((item: IFeatureJob) =>
-    item.categories.map((category) => category._id)
-  );
-});
-
-const findIdMatch = computed(() => {
-  const id = String(route.params.id);
-  return categories.value.find((category) => category.includes(id));
-});
-
+const id = String(route.params.id);
 const matchingFeatureJobs = computed(() => {
-  const id = String(findIdMatch.value);
-  if (!findIdMatch.value) return [];
-  return featureJobList.value.filter((job) =>
-    job.categories.some((category) => category._id === id)
+  if (!id) return [];
+  const matchingId = featureJobList.value.filter((job) =>
+    job.categories.map((cate) => cate._id).includes(id)
   );
+
+  return matchingId;
+});
+
+const matchingTitle = computed(() => {
+  if (!id) return "";
+  const title = matchingFeatureJobs.value[0]?.categories.find((cate) =>
+    cate._id.includes(id)
+  )?.name;
+  return title;
 });
 
 const fetchData = async () => {
